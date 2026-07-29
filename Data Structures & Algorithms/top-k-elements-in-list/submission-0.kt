2f5802e458e10831d84d61a2beedc6b08ed109c7
SHAWN LOPES <qwertytopo21@gmail.com>
2026-07-29 15:38:52 +0530

    Add: top-k-elements-in-list - submission-0

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-0.kt
@@ -0,0 +1,20 @@
+class Solution {
+    fun topKFrequent(nums: IntArray, k: Int): IntArray {
+        val map = HashMap<Int, Int>()
+        val newArr = mutableListOf<Int>()
+        for (num in nums) {
+          
+                map[num] = map.getOrDefault(num, 0) + 1
+           
+        
+        }
+
+             return map.entries
+            .sortedByDescending { it.value }
+            .take(k)
+            .map { it.key }
+            .toIntArray()
+
+    }
+}
+
